d8d086889b44c24a5090833e7f78542bd529515e
lukechampine <luke.champine@gmail.com>
2017-11-09 17:45:35 -0500

    export MaxPayloadSize

--- consts.go
+++ consts.go
@@ -4,7 +4,12 @@ const (
 	checksumSize   = 16
 	pageSize       = 4096
 	pageMetaSize   = checksumSize + 4*8 // checksum + 4 uint64s
-	maxPayloadSize = pageSize - pageMetaSize
+
+	// MaxPayloadSize is the number of bytes that can fit into a single
+	// page. For best performance, the number of pages written should be
+	// minimized, so clients should try to keep the length of an Update's
+	// Instructions field slightly below a multiple of MaxPayloadSize.
+	MaxPayloadSize = pageSize - pageMetaSize
 )
 
 const (
