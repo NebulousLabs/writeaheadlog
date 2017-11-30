03f8090f1c1d3a54d49e27cb9379cb4d63d326d6
David Vorick <david@nebulouslabs.com>
2017-11-29 22:06:04 -0500

    Change dips verification to also consider last->first element

--- writeaheadlog_integration_test.go
+++ writeaheadlog_integration_test.go
@@ -357,6 +357,12 @@ func verifyNumbers(numbers []uint32) error {
 			dips++
 		}
 	}
+	// Check for the dip from the last number to the first number.
+	if len(numbers) > 1 {
+		if numbers[len(numbers)-1] < numbers[0] {
+			dips++
+		}
+	}
 	if dips > 1 {
 		return fmt.Errorf("numbers are corrupted %v", numbers)
 	}
